011f377071520eb815414bbd2a728d63e7839de0
briangomez2016 <brian.gomez2016@outlook.com>
2018-05-31 19:35:46 -0300

    primer comit

--- app/src/main/java/com/example/brian/bicicletasusuario/RegistroUsuario.java
+++ app/src/main/java/com/example/brian/bicicletasusuario/RegistroUsuario.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 import com.example.brian.bicicletasusuario.ApiCliente.ApiCliente;
 import com.example.brian.bicicletasusuario.RespuestaRegistrar;
 import com.example.brian.bicicletasusuario.ApiInterface.ApiInterface;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -46,33 +47,52 @@ public class RegistroUsuario extends AppCompatActivity {
     RadioGroup radio;
 
 
-    public  void Registrarse(View v){
-         final ApiInterface api = ApiCliente.getClient().create(ApiInterface.class);
-                if (ci.isChecked()) {
-                    String nom = nombre.getText().toString();
-                    String em = email.getText().toString();
-                    String telefono = tel.getText().toString();
-                    String ci = documento.getText().toString();
-                    String password = pass.getText().toString();
-                    Call<RespuestaRegistrar> call = api.savePost(ci,"",nom,em,password,telefono,"lavalleja 1782");
-                    call.enqueue(new Callback<RespuestaRegistrar>() {
-                        @Override
-                        public void onResponse(Call<RespuestaRegistrar> call, Response<RespuestaRegistrar> response) {
-                            Toast.makeText(RegistroUsuario.this, "Bien", Toast.LENGTH_SHORT).show();
-                        }
-
-                        @Override
-                        public void onFailure(Call<RespuestaRegistrar> call, Throwable t) {
-                            Toast.makeText(RegistroUsuario.this,t.getMessage().toString(), Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                    nombre.setText("");
-                    email.setText("");
-                    tel.setText("");
-                    documento.setText("");
-                    }
+    public void Registrarse(View v) {
+        final ApiInterface api = ApiCliente.getClient().create(ApiInterface.class);
+        String nom = nombre.getText().toString();
+        String em = email.getText().toString();
+        String telefono = tel.getText().toString();
+        String password = pass.getText().toString();
+        String idMovil = FirebaseInstanceId.getInstance().getToken();
+        if (ci.isChecked()) {
+            String cedula = documento.getText().toString();
+            Call<RespuestaRegistrar> call = api.savePost(cedula, "", nom, em, password, telefono, "lavalleja 1782");
+            call.enqueue(new Callback<RespuestaRegistrar>() {
+                @Override
+                public void onResponse(Call<RespuestaRegistrar> call, Response<RespuestaRegistrar> response) {
+                    Toast.makeText(RegistroUsuario.this, "Bien", Toast.LENGTH_SHORT).show();
                 }
 
+                @Override
+                public void onFailure(Call<RespuestaRegistrar> call, Throwable t) {
+                    Toast.makeText(RegistroUsuario.this, t.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            nombre.setText("");
+            email.setText("");
+            tel.setText("");
+            documento.setText("");
+        } else {
+            String pasaporte = documento.getText().toString();
+            Call<RespuestaRegistrar> call = api.savePost("", pasaporte, nom, em, password, telefono, "lavalleja 1782");
+            call.enqueue(new Callback<RespuestaRegistrar>() {
+                @Override
+                public void onResponse(Call<RespuestaRegistrar> call, Response<RespuestaRegistrar> response) {
+                    Toast.makeText(RegistroUsuario.this, "Bien", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(Call<RespuestaRegistrar> call, Throwable t) {
+                    Toast.makeText(RegistroUsuario.this, t.getMessage().toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            nombre.setText("");
+            email.setText("");
+            tel.setText("");
+            documento.setText("");
+
+        }
+    }
 
 
     @Override
