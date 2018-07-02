3de881d8f0e045732831a86d4670f99d1888c974
alepeculio <alejandropeculio@gmail.com>
2018-07-02 19:40:08 -0300

    tipo de incidencia actualizado

--- app/src/main/java/com/example/brian/bicicletasusuario/ApiCliente/ApiCliente.java
+++ app/src/main/java/com/example/brian/bicicletasusuario/ApiCliente/ApiCliente.java
@@ -1,4 +1,5 @@
 package com.example.brian.bicicletasusuario.ApiCliente;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,17 +9,17 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiCliente {
 
-        public static final String BASE_URL = "http://192.168.10.56/BackEndMoviles/public/";
-        private static Retrofit retrofit = null;
+    public static final String BASE_URL = "http://192.168.10.97/BackEndMoviles/public/";
+    private static Retrofit retrofit = null;
 
 
-        public static Retrofit getClient() {
-                if (retrofit==null) {
-                        retrofit = new Retrofit.Builder()
-                                .baseUrl(BASE_URL)
-                                .addConverterFactory(GsonConverterFactory.create())
-                                .build();
-                }
-                return retrofit;
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
         }
+        return retrofit;
+    }
 }
