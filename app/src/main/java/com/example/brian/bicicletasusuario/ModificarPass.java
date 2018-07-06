d7600a47bc3909ed97bcd5e1db78f36d9a9c9252
Brian <brian.gomez2016@outlook.com>
2018-07-06 18:17:30 -0300

    Merge branch 'master' of https://github.com/briangomez2016/BicicletasUsuario
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/java/com/example/brian/bicicletasusuario/IniciarSesion.java
    #	app/src/main/java/com/example/brian/bicicletasusuario/RegistroUsuario.java
    #	app/src/main/res/layout/login.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/brian/bicicletasusuario/ModificarPass.java
+++ app/src/main/java/com/example/brian/bicicletasusuario/ModificarPass.java
@@ -1,16 +1,22 @@
 package com.example.brian.bicicletasusuario;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.brian.bicicletasusuario.ApiCliente.ApiCliente;
 import com.example.brian.bicicletasusuario.ApiInterface.ApiInterface;
 import com.example.brian.bicicletasusuario.ApiInterface.RespuestaCambiarContra;
 import com.example.brian.bicicletasusuario.ApiInterface.RespuestaCorreo;
+import com.example.brian.bicicletasusuario.ApiInterface.RespuestaUsuario;
+import com.example.brian.bicicletasusuario.Clases.Usuario;
 
 
 import butterknife.BindView;
@@ -32,35 +38,46 @@ public class ModificarPass extends AppCompatActivity {
     EditText codigo;
     @BindView(R.id.nuevoPass)
     EditText nuevopass;
-    @BindView(R.id.confirNuevo)
+    @BindView(R.id.confirmPass)
     EditText confirpass;
-    String correo;
-    String contraseña;
+    @BindView(R.id.cambiar)
+    LinearLayout pCambiar;
+    @BindView(R.id.recuperar)
+    LinearLayout pRecuperar;
+    String emailTemporal="";
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.modificarpass);
         ButterKnife.bind(this);
+        emailTemporal="";
+
     }
 
     public void enviarCorreo(View v){
         ApiInterface api = ApiCliente.getClient().create(ApiInterface.class);
-        String correo = email.getText().toString();
+        if(email.getText().toString().isEmpty()){
+            email.setError("Debe ingresar un email");
+            return;
+        }
+        final String correo = email.getText().toString();
         Call<RespuestaCorreo> call = api.GenerarCodigo(correo);
         call.enqueue(new Callback<RespuestaCorreo>() {
             @Override
             public void onResponse(Call<RespuestaCorreo> call, Response<RespuestaCorreo> response) {
                 if(response.body().getCodigo().equals("1")){
-                    SharedPreferences sp = getSharedPreferences ("RecuperarContraseña", MODE_PRIVATE);
-                    SharedPreferences.Editor et = sp.edit ();
-                    et.putString ("codigo", response.body().getCodigoGenerado());
+                    emailTemporal=correo;
+                    Log.d("Codigo", "onResponse: "+response.body().getCodigoGenerado());
+                }else{
+                    Log.d("Problema", "onResponse: "+response.body().getMensaje());
+
                 }
             }
 
             @Override
             public void onFailure(Call<RespuestaCorreo> call, Throwable t) {
-
+                Log.d("Error recuperar pass", "onFailure: "+t.getMessage());
             }
         });
 
@@ -68,45 +85,73 @@ public class ModificarPass extends AppCompatActivity {
     }
 
     public void verificarCodigo(View v){
-        String codi = codigo.getText().toString();
-        SharedPreferences sp = getSharedPreferences("RecuperarContraseña" , MODE_PRIVATE);
-        String CodigoGenerado = sp.getString("codigo",null);
-        //Cambiar por CodigoGenerado
-        if(codi.equals("1a2b3c")){
-            Toast.makeText(this, "Correcto Perro", Toast.LENGTH_SHORT).show();
-            nuevopass.setEnabled(true);
-            confirpass.setEnabled(true);
+        ApiInterface api = ApiCliente.getClient().create(ApiInterface.class);
+        if(codigo.getText().toString().isEmpty()){
+            codigo.setError("Debe ingresar un codigo");
+        }
+        if(email.getText().toString().isEmpty()){
+            email.setError("Debe ingresar un email");
+        }
+        if(!email.getText().toString().isEmpty() && !codigo.getText().toString().isEmpty()) {
+            final String codi = codigo.getText().toString();
+            Call<RespuestaUsuario> call = api.getPerfil(emailTemporal);
+            call.enqueue(new Callback<RespuestaUsuario>() {
+                @Override
+                public void onResponse(Call<RespuestaUsuario> call, Response<RespuestaUsuario> response) {
+                    Usuario u = response.body().getUsuario();
+                    if (u == null) {
+                        Toast.makeText(ModificarPass.this, "No se pudo cargar el perfil", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
 
-        }else{
-            codigo.setError("Codigo Incorrecto");
+                    if (u.getPass().equals(codi)) {
+                        pRecuperar.setVisibility(View.GONE);
+                        pCambiar.setVisibility(View.VISIBLE);
+
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<RespuestaUsuario> call, Throwable t) {
+
+                }
+            });
         }
 
     }
     public void CambiarPass(View v ){
         String nuevo =  nuevopass.getText().toString();
         String confirm  = confirpass.getText().toString();
+        if(nuevo.isEmpty()){
+            nuevopass.setError("Debe ingresar una contraseña");
+        }
+        if(confirm.isEmpty()){
+            confirpass.setError("Confirmar la contraseña");
+        }
         if(nuevo.equals(confirm)){
             ApiInterface api = ApiCliente.getClient().create(ApiInterface.class);
-            Call<RespuestaCambiarContra> call = api.cambiarPass(nuevo);
-            call.enqueue(new Callback<RespuestaCambiarContra>() {
+            Call<RespuestaUsuario> call = api.editarPerfil(null,null,emailTemporal,null,null,nuevo);
+            call.enqueue(new Callback<RespuestaUsuario>() {
                 @Override
-                public void onResponse(Call<RespuestaCambiarContra> call, Response<RespuestaCambiarContra> response) {
+                public void onResponse(Call<RespuestaUsuario> call, Response<RespuestaUsuario> response) {
                     if(response.body().getCodigo().equals("1")){
-                        Toast.makeText(ModificarPass.this, "Su contraseña ha sido cambiado con exito baby", Toast.LENGTH_SHORT).show();
-
+                        Toast.makeText(ModificarPass.this, "Su contraseña ha sido cambiado con exito", Toast.LENGTH_SHORT).show();
+                        emailTemporal="";
+                        Intent intent =  new Intent(ModificarPass.this,IniciarSesion.class);
+                        startActivity(intent);
                     }else{
                         Toast.makeText(ModificarPass.this, "FUCK", Toast.LENGTH_SHORT).show();
                     }
                 }
 
                 @Override
-                public void onFailure(Call<RespuestaCambiarContra> call, Throwable t) {
-                    Toast.makeText(ModificarPass.this, "BUEN PROBLEM BABY", Toast.LENGTH_SHORT).show();
+                public void onFailure(Call<RespuestaUsuario> call, Throwable t) {
+                    Toast.makeText(ModificarPass.this, " Problema servidor cambiar pass", Toast.LENGTH_SHORT).show();
                 }
             });
 
         }else{
-            confirpass.setError("Las contraseñas no coiniciden");
+            confirpass.setError("Las contraseñas no coinic  iden");
         }
 
 
